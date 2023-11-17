f7d2349ea13f3b2c1fa8e0b678e5ee97355c8160
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2023-11-17 16:29:17 +0900

    initial parser test for variable statement

---
+++ 4-Parser/parser/parser_test.go
@@ -0,0 +1,62 @@
+package parser
+
+import (
+	"interpreter/3-Lexer/lexer"
+	"interpreter/4-Parser/ast"
+	"testing"
+)
+
+func TestVarStatements(t *testing.T) {
+	input := `
+	var x = 12
+	var y = 60
+	var xyz = 123456
+	`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got %d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"xyz"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testVarStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testVarStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "var" {
+		t.Errorf("s.TokenLiteral not 'var'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	varStmt, ok := s.(*ast.VarStatement)
+
+	if !ok {
+		t.Errorf("s not *ast.VarStatement. got=%T", s)
+		return false
+	}
+
+	if varStmt.Name.Value != name {
+		t.Errorf("varStmt.Name.Value not '%s'. got=%s", name, varStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
+}
