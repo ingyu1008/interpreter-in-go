8d8fc2ae8be6c563d64ef56c957db38200bb76a0
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2023-11-17 15:43:21 +0900

    add parser structure

---
+++ 4-Parser/ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "interpreter/3-Lexer/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type VarStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (st *VarStatement) statementNode()       {}
+func (st *VarStatement) TokenLiteral() string { return st.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
---
+++ 4-Parser/parser/parser.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"interpreter/3-Lexer/lexer"
+	"interpreter/3-Lexer/token"
+	"interpreter/4-Parser/ast"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.NextToken()
+	p.NextToken()
+
+	return p
+}
+
+func (p *Parser) NextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
